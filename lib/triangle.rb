44a58255edb6b858f8fabd8800196f657290c033
Luke <luciusfow@gmail.com>
2022-06-25 22:42:41 -0400

    commit tree a6eb3bba69bd10c91dbf897d02b1cfaec7e988cb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a 
+    @b = b
+    @c = c
+  end
+
+  def kind
+    val_tri
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  def sides?
+    [a,b,c].all?(&:positive?)
+  end
+
+  def ineq?
+    a + b > c && a + c > b && b+c >a
+  end
+
+  def val_tri
+    raise TriangleError unless sides? && ineq?
+  end
+
+  class TriangleError < StandardError
+  end
 end
